cbc3590382cd40d9ea9dc5e50fca41922ab66c7f
Jennika Wilson <j.r.a.112100@gmail.com>
2022-07-20 21:27:56 -0400

    starting on posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Post from './Post/Post.js'
 import './posts-style.css'
 import {useSelector} from 'react-redux'
+import {Grid, CircularProgress} from '@mui/material'
 
 const Posts = () => {
     const posts  = useSelector((state) => state.posts)
@@ -9,11 +10,15 @@ const Posts = () => {
     console.log(posts)
 
     return (
-        <>
-            <h1>POSTS</h1>
-            <Post />
-            <Post />
-        </>     
+        !posts.length ? <CircularProgress /> : (
+            <Grid className='container' container alignItems='stretch' spacing={3} >
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     )
 }
 
