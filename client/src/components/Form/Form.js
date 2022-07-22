ade87a4cf0afa6457ca67bfb2c6571020cf063f5
Jennika Wilson <j.r.a.112100@gmail.com>
2022-07-21 22:51:07 -0400

    Fixed / finished update

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,39 +1,41 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import './form-styles.css'
-import FileBase from 'react-file-base64'
-import {TextField, Button, Typography, Paper} from '@mui/material'
-import {useDispatch, useSelector} from 'react-redux'
+import { TextField, Button, Typography, Paper } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import FileBase from 'react-file-base64';
 
-import { createPost, updatePost } from '../../actions/posts.js'
+import { createPost, updatePost } from '../../actions/posts';
 
-const Form = (currentId, setCurrentId) => {
-    const [postData, setPostData] = useState({creator: '', title: '', message: '', tags: '', selectedFile: ''})
-    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null)
-    const dispatch = useDispatch()
+const Form = ({ currentId, setCurrentId }) => {
+  const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        if(post) setPostData(post)
-    }, [post])
+  useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+  const clear = () => {
+    setCurrentId(0);
+    setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  };
 
-        if(currentId) {
-            dispatch(updatePost(currentId, postData))
-        } else {
-            dispatch(createPost(postData))
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    if (currentId === 0) {
+      dispatch(createPost(postData));
+      clear();
+    } else {
+      dispatch(updatePost(currentId, postData));
+      clear();
     }
-
-    const clear = () => {
-
-    }
+  };
 
     return (
        <Paper className='paper'>
         <form autoComplete='off' noValidate className='root form' onSubmit={handleSubmit}>
-            <Typography variant='h6'>Creating a Memory</Typography>
+            <Typography variant='h6'>{currentId ? 'Editing' : 'Creating'} a Memory</Typography>
             <TextField name='creator' variant='outlined' label='Creator' fullWidth value={postData.creator} onChange={(e) => setPostData({... postData, creator: e.target.value})} />
             <TextField name='title' variant='outlined' label='Title' fullWidth value={postData.title} onChange={(e) => setPostData({... postData, title: e.target.value})} />
             <TextField name='message' variant='outlined' label='Message' fullWidth value={postData.message} onChange={(e) => setPostData({... postData, message: e.target.value})} />
