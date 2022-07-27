441cd20c23d76103359a927637033b64151e11da
Jennika Wilson <j.r.a.112100@gmail.com>
2022-07-27 17:43:27 -0400

    Deploying

--- server/index.js
+++ server/index.js
@@ -15,8 +15,10 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-//connecting to mongodb
-const CONNECTION_URL = 'mongodb+srv://admin:jalspaw123@cluster0.t6xqsiq.mongodb.net/?retryWrites=true&w=majority'
+app.get('/', (req, res) => {
+    res.send('Welcome to memories API')
+})
+
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true}) 
